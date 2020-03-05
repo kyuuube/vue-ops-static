a2f158bda54620dd9959ac12b61c369eff32d721
kyuuube <benchiu@yandex.com>
2020-03-06 00:22:13 +0800

    ====

--- src/views/children/dashboard/components/SampleMiniChartBox.vue
+++ src/views/children/dashboard/components/SampleMiniChartBox.vue
@@ -29,19 +29,31 @@ export default {
     mounted() {
         this.$nextTick(() => {
             const data = [
-                { year: '1986', Compitor: 42 },
-                { year: '1987', Compitor: 54 },
-                { year: '1988', Compitor: 26 },
-                { year: '1989', Compitor: 32 },
-                { year: '1990', Compitor: 68 },
-                { year: '1991', Compitor: 54 },
-                { year: '1992', Compitor: 35 },
-                { year: '1993', Compitor: 66 },
+                { year: '1986', ACME: 162},
+                { year: '1987', ACME: 134},
+                { year: '1988', ACME: 116},
+                { year: '1989', ACME: 122},
+                { year: '1990', ACME: 178},
+                { year: '1991', ACME: 144},
+                { year: '1992', ACME: 125},
+                { year: '1993', ACME: 176},
+                { year: '1994', ACME: 156 },
+                { year: '1995', ACME: 195 },
+                { year: '1996', ACME: 215 },
+                { year: '1997', ACME: 176 },
+                { year: '1998', ACME: 167 },
+                { year: '1999', ACME: 142 },
+                { year: '2000', ACME: 117 },
+                { year: '2001', ACME: 113 },
+                { year: '2002', ACME: 132 },
+                { year: '2003', ACME: 146 },
+                { year: '2004', ACME: 169 },
+                { year: '2005', ACME: 184 }
             ];
             const dv = new View().source(data);
             dv.transform({
                 type: 'fold',
-                fields: [ 'Compitor' ],
+                fields: [ 'ACME' ],
                 key: 'type',
                 value: 'value'
             });
